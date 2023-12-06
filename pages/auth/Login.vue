64d3bbfa6262cefe00017d49bb75ae9bf5fd662f
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-12-06 17:34:11 +0900

    Login画面修正

--- pages/auth/Login.vue
+++ pages/auth/Login.vue
@@ -1,4 +1,3 @@
-
 <script setup lang="ts">
 import { Icon } from "@iconify/vue";
 
@@ -6,13 +5,15 @@ definePageMeta({
   layout: "auth",
 });
 
-const router = useRouter()
+const router = useRouter();
 const isLoading = ref(false);
 const isSignInDisabled = ref(false);
 
 const refLoginForm = ref();
 const email = ref("vuetify3-visitor@gmail.com");
 const password = ref("sfm12345");
+const shopCode = ref("");
+const userId = ref("");
 const isFormValid = ref(true);
 
 // show password field
@@ -31,14 +32,9 @@ const handleLogin = async () => {
     isLoading.value = true;
     isSignInDisabled.value = true;
     loginWithEmailAndPassword(email.value, password.value);
-
   } else {
     console.log("no");
   }
-};
-
-const signInWithGoolgle = () => {
-  loginWithGoogle();
 };
 
 // Error Check
@@ -63,65 +59,76 @@ const resetErrors = () => {
   error.value = false;
   errorMessages.value = "";
 };
-
-
 </script>
 <template>
   <v-card color="white" class="pa-3 ma-3" elevation="3">
     <v-card-title class="my-4 text-h4">
-      <span class="flex-fill"> Welcome </span>
+      <span class="flex-fill font-weight-black text-primary"> Xeus </span>
     </v-card-title>
-    <v-card-subtitle>Sign in to your account</v-card-subtitle>
+
     <!-- sign in form -->
 
     <v-card-text>
-      <v-form ref="refLoginForm" class="text-left" v-model="isFormValid" lazy-validation>
-        <v-text-field ref="refEmail" v-model="email" required :error="error" label="Email" density="default"
-          variant="underlined" color="primary" bg-color="#fff" :rules="emailRules" name="email" outlined validateOn="blur"
-          placeholder="403474473@qq.com" @keyup.enter="handleLogin" @change="resetErrors"></v-text-field>
-        <v-text-field ref="refPassword" v-model="password" :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="showPassword ? 'text' : 'password'" :error="error" :error-messages="errorMessages" label="Password"
-          placeholder="sfm12345" density="default" variant="underlined" color="primary" bg-color="#fff"
-          :rules="passwordRules" name="password" outlined validateOn="blur" @change="resetErrors"
-          @keyup.enter="handleLogin" @click:append-inner="showPassword = !showPassword"></v-text-field>
-        <v-btn :loading="isLoading" :disabled="isSignInDisabled" block size="x-large" color="primary" @click="handleLogin"
-          class="mt-2">Login</v-btn>
-
-        <div class="text-grey text-center text-caption font-weight-bold text-uppercase my-5">
-          or
-        </div>
-
-        <!-- external providers list -->
-        <v-btn class="mb-2 text-capitalize" color="white" elevation="1" block size="x-large" @click="signInWithGoolgle"
-          :disabled="isSignInDisabled">
-          <Icon icon="logos:google-icon" class="mr-3 my-2" />
-          Google
-        </v-btn>
-
+      <v-form
+        ref="refLoginForm"
+        class="text-left"
+        v-model="isFormValid"
+        lazy-validation
+      >
+        <v-text-field
+          v-model="shopCode"
+          variant="underlined"
+          label="店舗コード"
+          color="primary"
+        ></v-text-field>
+        <v-text-field
+          v-model="userId"
+          variant="underlined"
+          label="担当者ID"
+          color="primary"
+        ></v-text-field>
+        <v-text-field
+          ref="refPassword"
+          v-model="password"
+          :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="showPassword ? 'text' : 'password'"
+          :error="error"
+          :error-messages="errorMessages"
+          label="パスワード"
+          placeholder="sfm12345"
+          density="default"
+          variant="underlined"
+          color="primary"
+          bg-color="#fff"
+          :rules="passwordRules"
+          name="password"
+          outlined
+          validateOn="blur"
+          @change="resetErrors"
+          @keyup.enter="handleLogin"
+          @click:append-inner="showPassword = !showPassword"
+        ></v-text-field>
+        <v-btn
+          :loading="isLoading"
+          :disabled="isSignInDisabled"
+          block
+          size="x-large"
+          color="primary"
+          @click="handleLogin"
+          class="mt-2"
+          >ログイン</v-btn
+        >
 
         <div v-if="errorProvider" class="error--text my-2">
           {{ errorProviderMessages }}
         </div>
-
-        <div class="mt-5 text-center">
-          <router-link class="text-primary" to="/auth/forgot-password">
-            Forgot password
-          </router-link>
-        </div>
-      </v-form></v-card-text>
+      </v-form></v-card-text
+    >
   </v-card>
-  <div class="text-center mt-6">
-    Don't have an account yet?
-    <router-link to="/auth/signup" class="text-primary font-weight-bold">
-      Sign up
-    </router-link>
-  </div>
 </template>
-
-
 
 <style scoped lang="scss">
 .main-bg {
-  background-image: linear-gradient(to right, #D5DBE7, #D5DBE7);
+  background-image: linear-gradient(to right, #d5dbe7, #d5dbe7);
 }
 </style>
